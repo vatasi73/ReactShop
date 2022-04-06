2f6d5da3ebaaa7bc0a8a5cff0377cb8abcd6de25
Vatasi <tifu7311@yandex.ru>
2022-04-06 17:31:53 +0400

    gh-pages added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,12 @@ function Footer() {
       <div className="footer-copyright">
         <div className="container">
           © {new Date().getFullYear()} Copyright Text
-          <a className="grey-text text-lighten-4 right" href="#!">
+          <a
+            className="grey-text text-lighten-4 right"
+            href="https://vatasi73.github.io/ReactShop"
+            target="_blank"
+            rel="noreferrer"
+          >
             Repo
           </a>
         </div>
